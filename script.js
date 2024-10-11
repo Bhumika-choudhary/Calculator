a1005ec9ba944f416854adae3e974b2f57705e9f
Bhumika-choudhary <priyanshic14@gmail.com>
2024-10-11 18:06:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function myfunction() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let operator = document.getElementById('calculate').value;
+    let result;
+
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    } else if (operator === '*') {
+        result = num1 * num2;
+    } else if (operator === '/') {
+        result = num1 / num2;
+    }
+
+    document.getElementById('result').value = result;
+}
